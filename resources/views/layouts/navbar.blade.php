41d25d25b8e713be4578837533faab9f6254657b
daniruliansyah <daniruliansyah03@sma.belajar.id>
2023-07-18 23:13:58 +0700

    masih ada bug(bug url), edit data belum efisien, belum crud yang detele

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,8 @@
+<ul class="navbar">
+            <li><a href="/">HOME</a></li>
+            <li><a href="pekerja">PEKERJA</a></li>
+            <li><a href="login">LOGIN</a></li>
+            <li><a href="register">REGISTER</a></li>
+            <li><a href="art">ART</a></li>
+            <li><a href="babysitter">BABY SITTER</a></li>
+        </ul>
